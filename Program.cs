6cdd381f6a6a2fc396fc97ac7c6eeac8515b0751
SAMWEL-69 <samwelngari032@gmail.com>
2022-07-05 16:35:45 +0300

    For loop

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ifStatement{
+
+  class Program
+ {
+     static void Main(string[]args)
+     {
+    string inputStr=" ";
+    Console.WriteLine("Basic while() Loop:");
+while(inputStr!="exit"){
+    inputStr=Console.ReadLine();
+    Console.WriteLine("you entered {0}",inputStr);
+}
+        
+  }
+ }
+}
